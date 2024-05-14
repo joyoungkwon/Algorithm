d4631b9d69b3e5cc9e11d75a295053fd08abdb1f
whdudrnjsdlqslek <137140434+joyoungkwon@users.noreply.github.com>
2024-05-15 05:20:55 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,18 +1,12 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.Scanner;
-
+ 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-        Scanner s = new Scanner(System.in);
-       int fixed = 543;
-       int result = s.nextInt();
-
-        System.out.println(( result-fixed));
-    }
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int year = in.nextInt();
+		
+		System.out.println(year - 543);
+	}
 }
-
-
